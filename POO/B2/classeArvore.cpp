af5c57b7761ab903cc57d35155e0f9e95cc4274e
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2020-06-02 14:56:16 -0300

    corrigido script SO

--- POO/B2/classeArvore.cpp
+++ POO/B2/classeArvore.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
-#include<string.h>
-#include<stdio.h>
-#include<stdio_ext.h> // para usar o __fpurge(stdin) linux.
+  #ifdef _WIN32
+    #include<iostream>
+    #include<string.h>
+    #include<stdio.h>
+  #else
+    #include<iostream>
+    #include<string.h>
+    #include<stdio.h>
+    #include<stdio_ext.h> //para usar o __fpurge(stdin); (limpar buffer linux)
+  #endif
+
+
 using namespace std;
 
 class Arvore {
